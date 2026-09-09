f4b554181158b2c15852ea482acb9cc63c40bf34
Silas Martins de Sales <silasmsales@hotmail.com>
2026-09-09 17:59:50 -0300

    Fecha versão 1.0.0

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  staging: false,
+  name: 'producao',
+  supabaseUrl: 'https://jaaqsknyzxcbyygmpbey.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphYXFza255enhjYnl5Z21wYmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5ODIyNTEsImV4cCI6MjEwNDU1ODI1MX0.21c3sJpYg0kmiXusn7_oz18SyxFlwE2-qt5dj-TdPjQ'
+};
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  staging: true,
+  name: 'homologacao',
+  supabaseUrl: 'https://umrzmenylzqydjpyucvq.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtcnptZW55bHpxeWRqcHl1Y3ZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2MTE4MDIsImV4cCI6MjEwMzE4NzgwMn0.nNRtm2WQh_Mj0b0MVuWwpRB9XTshEuIBfxE4HpmTDrk'
+};
